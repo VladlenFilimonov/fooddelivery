35b9d47366e10b40150fd3f23f0f145b2528523c
Richard Kuodis <uberviolence@gmail.com>
2016-07-19 14:08:59 +0300

    Added testdbprintcontrollerservice

--- application-api/src/main/java/accenture/team3/fooddelivery/controllers/TestController.java
+++ application-api/src/main/java/accenture/team3/fooddelivery/controllers/TestController.java
@@ -1,20 +1,28 @@
 package accenture.team3.fooddelivery.controllers;
 
 import accenture.team3.fooddelivery.domain.Restaurant;
+import accenture.team3.fooddelivery.services.TestPrintDB;
 import accenture.team3.fooddelivery.services.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController {
 
     private TestService testService;
 
+    private TestPrintDB testPrintDB;
+
+
     @Autowired
-    public TestController(TestService testService) {
+    public TestController(TestService testService, TestPrintDB testPrintDB) {
         this.testService = testService;
+        this.testPrintDB = testPrintDB;
     }
 
     @RequestMapping("/add")
@@ -31,4 +39,17 @@ public class TestController {
         Restaurant rest = testService.getRestaurantById(Long.parseLong(id));
         return rest.getCompanyName();
     }
+
+    @RequestMapping("/printrestaurants")
+    public String printRestaurants(Model model) {
+        List<Restaurant> list = testPrintDB.getAllRestaurants();
+        System.out.println();
+//        list.forEach( (r) -> System.out.println(r));
+        model.addAttribute("restaurants", list);
+        return "dbrestauranttable";
+    }
+
+
+
+
 }

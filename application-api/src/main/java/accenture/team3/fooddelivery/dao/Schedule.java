efad5c1ae3abd125d4d194489660b21a1ed9098c
RichardK <uberviolence@gmail.com>
2016-07-22 13:32:18 +0300

    dao

---
+++ application-api/src/main/java/accenture/team3/fooddelivery/dao/Schedule.java
@@ -0,0 +1,9 @@
+package accenture.team3.fooddelivery.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by UberV on 22.07.2016.
+ */
+public interface Schedule extends CrudRepository<Schedule, Long> {
+}

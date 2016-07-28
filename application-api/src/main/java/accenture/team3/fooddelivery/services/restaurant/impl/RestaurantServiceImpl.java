0273d457e1cc6ca1d40a9fa76bf2016aaf0334dc
Vladlens Filimonovs <vladlenfilimonovs@gmail.com>
2016-07-28 14:32:06 +0300

    Comment Service and Controller

--- application-api/src/main/java/accenture/team3/fooddelivery/services/restaurant/impl/RestaurantServiceImpl.java
+++ application-api/src/main/java/accenture/team3/fooddelivery/services/restaurant/impl/RestaurantServiceImpl.java
@@ -3,7 +3,6 @@ package accenture.team3.fooddelivery.services.restaurant.impl;
 import accenture.team3.fooddelivery.dao.RestaurantDao;
 import accenture.team3.fooddelivery.domain.Rating;
 import accenture.team3.fooddelivery.domain.Restaurant;
-import accenture.team3.fooddelivery.services.comment.CommentService;
 import accenture.team3.fooddelivery.services.restaurant.RestaurantService;
 import accenture.team3.fooddelivery.shared.restaurant.RestaurantGetDto;
 import lombok.extern.slf4j.Slf4j;
@@ -17,12 +16,10 @@ import java.util.stream.Collectors;
 public class RestaurantServiceImpl implements RestaurantService {
 
     private RestaurantDao dao;
-    private CommentService commentService;
 
     @Autowired
-    public RestaurantServiceImpl(RestaurantDao dao, CommentService commentService) {
+    public RestaurantServiceImpl(RestaurantDao dao) {
         this.dao = dao;
-        this.commentService = commentService;
     }
 
     @Override
